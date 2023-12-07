83e9a70cde9ee7dc08cc3ceb011733fc8f4e310f
Tan Vo <tanvo149@outlook.com>
2023-12-07 17:09:58 +0000

    try again

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="Tan", version="1.0", packages=find_packages())
+setup(name="Tan", 
+    version="1.0", 
+    packages=find_packages(
+    where='data'),
+    package_dir={"" : "data"},
+    python_requires=">=3.8",)
+
