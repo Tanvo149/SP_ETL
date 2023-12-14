4ad9eb7bc61edc5b26d1ad544cffa8c97461829c
Tan Vo <tanvo149@outlook.com>
2023-12-14 17:45:05 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name="Tan", version="1.0", packages=find_packages(), python_requires=">=3.8")
+setup(name="Tan", 
+      version="1.0.0", 
+      python_requires=">=3.8",
+      packages=find_packages(include=["data", "data.*"]),
+      install_requires=[
+          'pandas>=2.0',
+          'SQLAlchemy>=2.0',
+          'psycopg2-binary>=2.0',
+          'pytest>=7.0'
+      ]
+)
